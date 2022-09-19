bac3bb88078b0d15a329107e4ec8d23f27cea27b
adanilova-tecracer <113897283+adanilova-tecracer@users.noreply.github.com>
2022-09-19 15:28:34 +0200

    Add files via upload

---
+++ cloudfront.tf
@@ -0,0 +1,58 @@
+# defining the name of the origin in our case the s3 bucket
+locals {
+  s3_origin_id = "static-s3-website"
+}
+
+# creating an oai for the cloudfront distribution
+resource aws_cloudfront_origin_access_identity "s3-oai" {
+    comment = "OAI for CloudFront"
+}
+
+# creating the cloudfront distribution
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.static_website_bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3-oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CloudFront providing HTTPS for static s3 website"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["DE"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
